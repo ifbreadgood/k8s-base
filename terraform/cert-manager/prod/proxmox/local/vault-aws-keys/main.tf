ccc1ca90934c2abdf82c0d1261ae41ff007a4e27
ifbread <ifbread@pm.me>
2025-07-14 00:18:05 -0400

    fix cert manager permissions

--- terraform/cert-manager/prod/proxmox/local/vault-aws-keys/main.tf
+++ terraform/cert-manager/prod/proxmox/local/vault-aws-keys/main.tf
@@ -20,43 +20,46 @@ data "vault_auth_backend" "this" {
   path = "kubernetes"
 }
 
-resource "vault_aws_secret_backend_role" "this" {
-  backend         = "aws"
-  name            = "route53-dns-challenge"
-  credential_type = "iam_user"
-  policy_document = <<EOT
-    {
-      "Version": "2012-10-17",
-      "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "route53:ListHostedZones",
-            "route53:GetChange"
-          ],
-          "Resource": ["*"]
-        },
-        {
-          "Effect" : "Allow",
-          "Action" : ["route53:ChangeResourceRecordSets"],
-          "Resource" : ["arn:aws:route53:::hostedzone/Z06285332QCWSWF1RDQBK"]
-        }
-      ]
-    }
-    EOT
-}
+# resource "vault_aws_secret_backend_role" "this" {
+#   backend         = "aws"
+#   name            = "route53-dns-challenge"
+#   credential_type = "iam_user"
+#   policy_document = <<EOT
+#     {
+#       "Version": "2012-10-17",
+#       "Statement": [
+#         {
+#           "Effect": "Allow",
+#           "Action": [
+#             "route53:ListHostedZones",
+#             "route53:GetChange"
+#           ],
+#           "Resource": ["*"]
+#         },
+#         {
+#           "Effect" : "Allow",
+#           "Action" : ["route53:ChangeResourceRecordSets"],
+#           "Resource" : ["arn:aws:route53:::hostedzone/Z06285332QCWSWF1RDQBK"]
+#         }
+#       ]
+#     }
+#     EOT
+# }
 
 resource "vault_policy" "this" {
   name = var.name
 
   policy = <<-EOT
-    path "${vault_aws_secret_backend_role.this.backend}/creds/${vault_aws_secret_backend_role.this.name}" {
-      capabilities = ["read"]
-    }
     path "pki_int/*" {
       capabilities = ["read"]
     }
     EOT
+
+  # policy = <<-EOT
+  #   path "${vault_aws_secret_backend_role.this.backend}/creds/${vault_aws_secret_backend_role.this.name}" {
+  #     capabilities = ["read"]
+  #   }
+  #   EOT
 }
 
 resource "vault_kubernetes_auth_backend_role" "this" {
--- terraform/cert-manager/prod/proxmox/local/vault-aws-keys/terragrunt.hcl
+++ terraform/cert-manager/prod/proxmox/local/vault-aws-keys/terragrunt.hcl
@@ -1,5 +1,14 @@
 include "backend" {
   path = find_in_parent_folders("aws-backend.hcl")
+  expose = true
+}
+
+locals {
+  tf_root = include.backend.locals.path_from_include
+}
+
+dependencies {
+  paths = ["${local.tf_root}/vault/prod/proxmox/local/auth-kubernetes"]
 }
 
 inputs = {
