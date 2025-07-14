3792cccce631cae6836b7cd132909cc85aff08cb
ifbread <ifbread@pm.me>
2025-07-14 04:05:07 -0400

    simplify argocd vault resource

--- terraform/cert-manager/prod/proxmox/local/vault-aws-keys/main.tf
+++ terraform/cert-manager/prod/proxmox/local/vault-aws-keys/main.tf
@@ -16,58 +16,10 @@ variable "name" {
   type = string
 }
 
-data "vault_auth_backend" "this" {
-  path = "kubernetes"
-}
-
-# resource "vault_aws_secret_backend_role" "this" {
-#   backend         = "aws"
-#   name            = "route53-dns-challenge"
-#   credential_type = "iam_user"
-#   policy_document = <<EOT
-#     {
-#       "Version": "2012-10-17",
-#       "Statement": [
-#         {
-#           "Effect": "Allow",
-#           "Action": [
-#             "route53:ListHostedZones",
-#             "route53:GetChange"
-#           ],
-#           "Resource": ["*"]
-#         },
-#         {
-#           "Effect" : "Allow",
-#           "Action" : ["route53:ChangeResourceRecordSets"],
-#           "Resource" : ["arn:aws:route53:::hostedzone/Z06285332QCWSWF1RDQBK"]
-#         }
-#       ]
-#     }
-#     EOT
-# }
-
-resource "vault_policy" "this" {
-  name = var.name
-
-  policy = <<-EOT
-    path "pki_int/*" {
-      capabilities = ["read"]
-    }
-    EOT
-
-  # policy = <<-EOT
-  #   path "${vault_aws_secret_backend_role.this.backend}/creds/${vault_aws_secret_backend_role.this.name}" {
-  #     capabilities = ["read"]
-  #   }
-  #   EOT
-}
-
 resource "vault_kubernetes_auth_backend_role" "this" {
-  # backend = data.vault_auth_backend.this
-  bound_service_account_names      = ["vault-${var.name}"]
+  bound_service_account_names      = ["vault-${var.name}", var.name]
   bound_service_account_namespaces = [var.name]
   role_name                        = var.name
-  token_policies                   = [vault_policy.this.name]
   audience                         = "external-secrets-operator"
   token_ttl                        = 60 * 60 * 24
 }
